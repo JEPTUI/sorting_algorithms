83670ccd317aec1a3ded2ec708b75c1035e28080
JEPTUI <alicejeptui@gmail.com>
2023-04-26 01:01:23 -0700

    Betty description

--- deck.h
+++ deck.h
@@ -4,6 +4,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * enum kind_e - represents deck of cards
+ * @SPADE: spade suit
+ * @HEART: heart suit
+ * @CLUB: club suit
+ * @DIAMOND: diamond suit
+ */
 typedef enum kind_e
 {
 	SPADE = 0,

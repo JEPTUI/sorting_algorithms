d0196b86c1b41fbe73598a5a96f5cef3fe58ad46
JEPTUI <alicejeptui@gmail.com>
2023-04-25 19:32:58 -0700

    Quick sort hoare

--- sort.h
+++ sort.h
@@ -40,5 +40,7 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void swap_sort(int *i, int *j);
 
 #endif
